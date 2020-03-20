1a342080b47ae9320d4b504948cd385243db68cb
Josh <140453a@acadiau.ca>
2020-03-20 13:16:32 -0300

    Added active location tracking to navbar.

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,21 +1,35 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
-import { Navbar, Nav} from 'react-bootstrap'
+import { withRouter } from 'react-router-dom';
+import { Navbar, Nav} from 'react-bootstrap';
+import { PropTypes } from 'prop-types';
 
 class Header extends Component {
-render() {
-    return (
-      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link as={Link}  to="/gallery">Gallery</Nav.Link>
-            <Nav.Link as={Link}  to="/trainer">Trainer</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-    );
-  }
+  render() {
+      const {match, location, history} = this.props;
+      return (
+        <React.Fragment>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <Nav.Link as={Link}  to="/gallery">Gallery</Nav.Link>
+              <Nav.Link as={Link}  to="/trainer">Trainer</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+        <div>
+          <h1>You are at {location.pathname}</h1>
+        </div>
+        </React.Fragment>
+
+
+      );
+    }
 }
-export default Header;
+
+Header.propTypes = {
+  location: PropTypes.object.isRequired,
+};
+export default withRouter(Header);
